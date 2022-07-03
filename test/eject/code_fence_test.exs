69056855262ffe83a81196c47629d4318205beea
Paul Statezny <paulstatezny@gmail.com>
2022-07-02 21:52:17 -0700

    Add more CodeFence tests

--- test/eject/code_fence_test.exs
+++ test/eject/code_fence_test.exs
@@ -14,7 +14,7 @@ defmodule Eject.CodeFenceTest do
         extra: []
       )
 
-    %{app: App.new!(project, manifest, TestApp)}
+    %{app: App.new!(project, manifest, CodeFenceApp)}
   end
 
   test "eject:lib", %{app: app} do
@@ -35,5 +35,74 @@ defmodule Eject.CodeFenceTest do
 
     assert output =~ "Keep"
     refute output =~ "Remove"
+    # code fences themselves are always removed
+    refute output =~ "eject"
+  end
+
+  test "eject:mix", %{app: app} do
+    output =
+      CodeFence.apply_fences(
+        """
+        defmodule Testing do
+          # <eject:mix:included_mix>
+          # Keep
+          # </eject:mix:included_mix>
+          # <eject:mix:excluded_mix>
+          # Remove
+          # </eject:mix:excluded_mix>
+        end
+        """,
+        app
+      )
+
+    assert output =~ "Keep"
+    refute output =~ "Remove"
+    # code fences themselves are always removed
+    refute output =~ "eject"
+  end
+
+  test "eject:app", %{app: app} do
+    # prime String.to_existing_atom
+    :code_fence_app
+    :another_app
+    output =
+      CodeFence.apply_fences(
+        """
+        defmodule Testing do
+          # <eject:app:code_fence_app>
+          # Keep
+          # </eject:app:code_fence_app>
+          # <eject:app:another_app>
+          # Remove
+          # </eject:app:another_app>
+        end
+        """,
+        app
+      )
+
+    assert output =~ "Keep"
+    refute output =~ "Remove"
+    # code fences themselves are always removed
+    refute output =~ "eject"
+  end
+
+  test "eject:remove", %{app: app} do
+    output =
+      CodeFence.apply_fences(
+        """
+        defmodule Testing do
+          # Keep
+          # <eject:remove>
+          # Remove
+          # </eject:remove>
+        end
+        """,
+        app
+      )
+
+    assert output =~ "Keep"
+    refute output =~ "Remove"
+    # code fences themselves are always removed
+    refute output =~ "eject"
   end
 end
