165631d816f2e54d5491ed1396d307a6de077035
Paul Statezny <paulstatezny@gmail.com>
2022-08-15 12:44:32 -0700

    Fix test mix.exs

--- test/projects/empty/mix.exs
+++ test/projects/empty/mix.exs
@@ -5,7 +5,7 @@ defmodule Empty.MixProject do
     [
       app: :empty,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
