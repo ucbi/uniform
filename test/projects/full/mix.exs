e2814f84ccda7a055c8eaf5dd6cbe68995e2f514
Paul Statezny <paulstatezny@gmail.com>
2022-08-15 12:47:48 -0700

    Correct version in mix.exs

--- test/projects/full/mix.exs
+++ test/projects/full/mix.exs
@@ -5,7 +5,7 @@ defmodule Full.MixProject do
     [
       app: :full,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
