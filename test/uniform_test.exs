d991a51bd23034998d4b13c00a891316798327bf
Paul Statezny <paulstatezny@gmail.com>
2022-08-15 13:26:06 -0700

    Run formatter

--- test/uniform_test.exs
+++ test/uniform_test.exs
@@ -13,11 +13,19 @@ defmodule UniformTest do
   end
 
   test "ejectable_apps/0" do
-    command = "Uniform.ejectable_apps() |> hd() |> Map.delete(:__struct__) |> inspect() |> IO.puts()"
+    command =
+      "Uniform.ejectable_apps() |> hd() |> Map.delete(:__struct__) |> inspect() |> IO.puts()"
+
     {stdout, 0} = System.cmd("mix", ["run", "-e", command])
     {map, []} = Code.eval_string(stdout)
     app = Map.put(map, :__struct__, Uniform.App)
     assert app.extra == [company: :fake_co, logo_file: "pixel"]
-    assert app.name == %{camel: "Tweeter", hyphen: "tweeter", module: Tweeter, underscore: "tweeter"}
+
+    assert app.name == %{
+             camel: "Tweeter",
+             hyphen: "tweeter",
+             module: Tweeter,
+             underscore: "tweeter"
+           }
   end
 end
