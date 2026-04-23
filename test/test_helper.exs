2c886b30de9a7ea50c35efc5a7b65228d49166d9
Andrew P. MacDonald <macdonald.andrew.p@gmail.com>
2026-04-23 11:53:34 -0500

    update test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,11 @@
-for dir <- ["full", "initable", "empty", "missing_template_dir", "missing_template"] do
+for dir <- [
+      "full",
+      "initable",
+      "empty",
+      "extra_formatting",
+      "missing_template_dir",
+      "missing_template"
+    ] do
   System.cmd("mix", ["deps.get"], cd: "test/projects/#{dir}")
   System.cmd("mix", ["compile"], cd: "test/projects/#{dir}")
 end
